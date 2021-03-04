b7554679179952e9ca0c9ee3fd179a9d3f64eea1
Armine Bajrami <abajrami317@gmail.com>
2021-03-04 16:27:14 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,10 @@
-# Define the variable board below.
+row=["1","2","3"]
+#position 1: Left
+row["0"]
+#posisition 2: Middle
+row["1"]
+#posisition 3: right
+row["2"]
+row["1"]= "x"
+row_2 =["4","5","6"]
+row_3 =["7","8","9"]
