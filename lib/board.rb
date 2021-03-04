4ba8b2ff70a033eae95be69e01595218dc8c9623
Armine Bajrami <abajrami317@gmail.com>
2021-03-04 16:38:50 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,7 @@
-row=["1","2","3"]
-#position 1: Left
+row=["1","2","3","4","5","6","7","8","9"]
 row["0"]
-#posisition 2: Middle
 row["1"]
-#posisition 3: right
 row["2"]
 row["1"]= "x"
-row_2 =["4","5","6"]
-row_3 =["7","8","9"]
+
+
